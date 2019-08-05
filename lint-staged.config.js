c6b32f53a380b3e9ccbff6b509510db6feb19c86
tessa <tessaSAC@users.noreply.github.com>
2019-08-05 15:55:36 -0400

    try something new with env vars

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
-}
+module.exports = process.env.PRE_COMMIT
+  ? {
+    '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
+  }
+
+  : {
+    '*.js': [
+      'yarn lint',
+    ],
+    // '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+    //   'yarn lint::prettier --parser json',
+    //   'git add',
+    // ],
+    // 'package.json': ['yarn lint:prettier', 'git add'],
+    // '*.vue': [
+    //   'yarn lint',
+    //   'git add',
+    // ],
+    // '*.scss': ['yarn lint:prettier', 'git add'],
+    // '*.md': ['yarn lint:prettier', 'git add'],
+  }
