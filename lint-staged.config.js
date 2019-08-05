6c742d1dfeca2253ce4237c5c9fe52f9ffdc85f4
tessa <tessaSAC@users.noreply.github.com>
2019-08-05 16:00:22 -0400

    test running lint directly

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,22 +1,3 @@
-console.dir(module.exports)
-module.exports = process.env.PRE_COMMIT
-  ? {
-    '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
-  }
-
-  : {
-    '*.js': [
-      'yarn lint',
-    ],
-    '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
-      'yarn lint::prettier --parser json',
-      'git push',
-    ],
-    'package.json': ['yarn lint:prettier', 'git push'],
-    '*.vue': [
-      'yarn lint',
-      'git push',
-    ],
-    '*.scss': ['yarn lint:prettier', 'git push'],
-    '*.md': ['yarn lint:prettier', 'git push'],
-  }
+module.exports = {
+  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
+}
