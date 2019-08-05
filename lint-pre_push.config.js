a59cf5d3caaa5533411d1cf4e754e329d43af0b9
tessa <tessaSAC@users.noreply.github.com>
2019-08-05 15:43:52 -0400

    testing lint-pre_push

--- lint-pre_push.config.js
+++ lint-pre_push.config.js
@@ -2,15 +2,15 @@ module.exports = {
   '*.js': [
     'yarn lint',
   ],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
-    'yarn lint::prettier --parser json',
-    'git add',
-  ],
-  'package.json': ['yarn lint:prettier', 'git add'],
-  '*.vue': [
-    'yarn lint',
-    'git add',
-  ],
-  '*.scss': ['yarn lint:prettier', 'git add'],
-  '*.md': ['yarn lint:prettier', 'git add'],
+  // '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+  //   'yarn lint::prettier --parser json',
+  //   'git add',
+  // ],
+  // 'package.json': ['yarn lint:prettier', 'git add'],
+  // '*.vue': [
+  //   'yarn lint',
+  //   'git add',
+  // ],
+  // '*.scss': ['yarn lint:prettier', 'git add'],
+  // '*.md': ['yarn lint:prettier', 'git add'],
 }
